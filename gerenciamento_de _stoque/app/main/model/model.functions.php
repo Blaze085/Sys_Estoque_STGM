35940f2ab381395b3f3ba097846007bd993c4b36
Blaze085 <162757165+Blaze085@users.noreply.github.com>
2025-05-07 08:02:55 -0300

    funcionalidade do button de exportar para pdf

--- gerenciamento_de _stoque/app/main/model/model.functions.php
+++ gerenciamento_de _stoque/app/main/model/model.functions.php
@@ -535,12 +535,13 @@ class gerenciamento
         echo '</div>
             
             <div class="mt-8 flex justify-center w-full">
+            <a href="../view/relatorios.php">
                 <button id="exportarBtn" class="bg-primary text-white font-bold py-3 px-8 rounded-lg hover:bg-opacity-90 transition-colors flex items-center shadow-md">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                     </svg>
                     Exportar para PDF
-                </button>
+                </button></a>
             </div>
             
             <!-- Modal de Edição -->
